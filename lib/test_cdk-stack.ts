a17516c200f24cdbaa4c49f4f70c38e6a22de927
TheRealRaziandJaso <Neno701@outlook.com>
2024-08-16 13:09:12 +1000

    detailed vpc layout

--- lib/test_cdk-stack.ts
+++ lib/test_cdk-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
-export class VpcStack extends cdk.Stack {
+export class testCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
