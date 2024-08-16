93de9c37aa3848fa752b704ea2b352a54d9fcb74
TheRealRaziandJaso <Neno701@outlook.com>
2024-08-16 10:07:18 +1000

    simple vpc code

--- lib/test_cdk-stack.ts
+++ lib/test_cdk-stack.ts
@@ -1,16 +1,40 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TestCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'MyVpc', {
+      cidr: '10.0.0.0/16',
+      maxAzs: 2,
+      natGateways: 1,
+      subnetConfiguration: [//THIS IS FOR BOTH AVAILABILITY ZONES
+        {
+          //WILL HAVE NAT GATEWAY AND ALB HERE
+          subnetType: ec2.SubnetType.PUBLIC,
+          name: 'PublicSubnet',
+          cidrMask: 24,
+        },
+        {
+          //FARGATE TASKS IN THESE 2
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          name: 'PrivateSubnetA',
+          cidrMask: 24,
+        },
+        {
+          //AURORA DB AND REPLICA IN THESE 2
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+          name: 'PrivateSubnetC',
+          cidrMask: 24,
+        },
+      ]
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TestCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new cdk.CfnOutput(this, 'VpcId', { //LEVEL 1 CONSTRUCT
+      value: vpc.vpcId
+    });
   }
 }
