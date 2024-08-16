c75ad82f5e1efad020ec22cefca3df8b25e70343
TheRealRaziandJaso <Neno701@outlook.com>
2024-08-16 10:48:46 +1000

    testing 3 new subnets

--- lib/test_cdk-stack.ts
+++ lib/test_cdk-stack.ts
@@ -11,7 +11,7 @@ export class TestCdkStack extends cdk.Stack {
       cidr: '10.0.0.0/16',
       maxAzs: 2,
       natGateways: 1,
-      subnetConfiguration: [//THIS IS FOR BOTH AVAILABILITY ZONES
+      /*subnetConfiguration: [//THIS IS FOR BOTH AVAILABILITY ZONES
         {
           //WILL HAVE NAT GATEWAY AND ALB HERE
           subnetType: ec2.SubnetType.PUBLIC,
@@ -30,7 +30,31 @@ export class TestCdkStack extends cdk.Stack {
           name: 'PrivateSubnetC',
           cidrMask: 24,
         },
-      ]
+      ]*/
+    });
+    //const az1 = 'ap-southeast-2a';
+    //const az2 = 'ap-southeast-2b';
+    const az1 = vpc.availabilityZones[0];
+    const az2 = vpc.availabilityZones[1];
+
+    const public_subnet = new ec2.PublicSubnet(this, 'Public_Subnet', {
+      vpcId: vpc.vpcId,
+      cidrBlock: '10.0.1.0/24',
+      availabilityZone: az1,
+      //mapPublicIpOnLaunch: true, //associates an ip to an instance at launch
+    });
+
+    const private_subnet_a_1 = new ec2.PrivateSubnet(this, "Private_Subnet_A_1", {
+      vpcId: vpc.vpcId,
+      cidrBlock: '10.0.2.0/24',
+      availabilityZone: az1,
+    });
+
+    //THIS ONE NEEDS INTERNET CONNECTIVITY, WHERE ECS INSTANCES WILL BE
+    const private_subnet_a_2 = new ec2.PrivateSubnet(this, "Private_Subnet_A_2", {
+      vpcId: vpc.vpcId,
+      cidrBlock: '10.0.3.0/24',
+      availabilityZone: az1,
     });
 
     new cdk.CfnOutput(this, 'VpcId', { //LEVEL 1 CONSTRUCT
