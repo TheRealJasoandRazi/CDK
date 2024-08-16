106d069fd526198ad4971ec092d967e466e2d104
TheRealRaziandJaso <Neno701@outlook.com>
2024-08-16 11:41:25 +1000

    mix of level 2 adn 1 constructs

--- lib/test_cdk-stack.ts
+++ lib/test_cdk-stack.ts
@@ -1,8 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import { ViaServicePrincipal } from 'aws-cdk-lib/aws-kms';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class TestCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -11,97 +9,38 @@ export class TestCdkStack extends cdk.Stack {
     const vpc = new ec2.Vpc(this, 'MyVpc', {
       cidr: '10.0.0.0/16',
       maxAzs: 1,
-      //natGateways: 1,
-      /*subnetConfiguration: [//THIS IS FOR BOTH AVAILABILITY ZONES
+      subnetConfiguration: [
         {
-          //WILL HAVE NAT GATEWAY AND ALB HERE
           subnetType: ec2.SubnetType.PUBLIC,
           name: 'PublicSubnet',
           cidrMask: 24,
         },
         {
-          //FARGATE TASKS IN THESE 2
           subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
           name: 'PrivateSubnetA',
           cidrMask: 24,
         },
         {
-          //AURORA DB AND REPLICA IN THESE 2
           subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
           name: 'PrivateSubnetC',
           cidrMask: 24,
         },
-      ]*/
+      ],
     });
+
     const internetGateway = new ec2.CfnInternetGateway(this, 'MyInternetGateway', {});
+
     new ec2.CfnVPCGatewayAttachment(this, 'MyVpcGatewayAttachment', {
       vpcId: vpc.vpcId,
-      internetGatewayId: internetGateway.ref
-    });
-    //const az1 = 'ap-southeast-2a';
-    //const az2 = 'ap-southeast-2b';
-    const az1 = vpc.availabilityZones[0];
-    //const az2 = vpc.availabilityZones[1];
-
-    const privateRouteTable = new ec2.CfnRouteTable(this, 'PrivateRouteTable', {
-      vpcId: vpc.vpcId,
+      internetGatewayId: internetGateway.ref,
     });
 
-    const privateRouteTable_WithNATGateway = new ec2.CfnRouteTable(this, 'PrivateRouteTable_WithNATGateway', {
-      vpcId: vpc.vpcId,
-    });
+    const publicSubnet = vpc.publicSubnets[0];
+    const privateSubnetA1 = vpc.privateSubnets[0];
+    const privateSubnetA2 = vpc.privateSubnets[1];
 
     const publicRouteTable = new ec2.CfnRouteTable(this, 'PublicRouteTable', {
       vpcId: vpc.vpcId,
-    });
-
-    ///////////////////////////////////////////////////////////////////
-    /////////////////////  AVAILABILITY ZONE A  ///////////////////////
-    ///////////////////////////////////////////////////////////////////
-    const public_subnet = new ec2.PublicSubnet(this, 'Public_Subnet', {
-      vpcId: vpc.vpcId,
-      cidrBlock: '10.0.1.0/24', //this is depreciated, use IP address instead
-      availabilityZone: az1,
-      //mapPublicIpOnLaunch: true, //associates an ip to an instance at launch
-    });
-
-    const private_subnet_a_1 = new ec2.PrivateSubnet(this, "Private_Subnet_A_1", {
-      vpcId: vpc.vpcId,
-      cidrBlock: '10.0.2.0/24',
-      availabilityZone: az1,
-    });
-
-    //THIS ONE NEEDS INTERNET CONNECTIVITY, WHERE ECS INSTANCES WILL BE
-    const private_subnet_a_2 = new ec2.PrivateSubnet(this, "Private_Subnet_A_2", {
-      vpcId: vpc.vpcId,
-      cidrBlock: '10.0.3.0/24',
-      availabilityZone: az1,
-    });
-
-    new ec2.CfnSubnetRouteTableAssociation(this, 'PrivateSubnetAssociation', {
-      subnetId: private_subnet_a_1.subnetId,
-      routeTableId: privateRouteTable.ref,
-    });
-
-    new ec2.CfnSubnetRouteTableAssociation(this, "PublicSubnetAssociation", {
-      subnetId: public_subnet.subnetId,
-      routeTableId: publicRouteTable.ref
-    })
-
-    const nat_gateway = new ec2.CfnNatGateway(this, "Nat_Gateway", {
-      subnetId: public_subnet.subnetId,
-      allocationId: new ec2.CfnEIP(this, 'EIP', {}).ref, // Create an Elastic IP for the NAT Gateway
-    });
-
-    new ec2.CfnSubnetRouteTableAssociation(this, 'PrivateSubnetAssociation_WithNATGateway', {
-      subnetId: private_subnet_a_2.subnetId,
-      routeTableId: privateRouteTable_WithNATGateway.ref,
-    });
-
-    new ec2.CfnRoute(this, 'RouteToNatGateway', {
-      routeTableId: privateRouteTable_WithNATGateway.ref,
-      destinationCidrBlock: '0.0.0.0/0',
-      natGatewayId: nat_gateway.ref
     });
 
     new ec2.CfnRoute(this, 'RouteToInternetGateway', {
@@ -109,9 +48,49 @@ export class TestCdkStack extends cdk.Stack {
       destinationCidrBlock: '0.0.0.0/0',
       gatewayId: internetGateway.ref,
     });
+
+    new ec2.CfnSubnetRouteTableAssociation(this, 'PublicSubnetAssociation', {
+      subnetId: publicSubnet.subnetId,
+      routeTableId: publicRouteTable.ref,
+    });
+
+    const eip = new ec2.CfnEIP(this, 'EIP', {});
     
-    new cdk.CfnOutput(this, 'VpcId', { //LEVEL 1 CONSTRUCT
-      value: vpc.vpcId
+    const natGateway = new ec2.CfnNatGateway(this, 'NatGateway', {
+      subnetId: publicSubnet.subnetId,
+      allocationId: eip.ref,
+    });
+
+    const privateRouteTableWithNATGateway = new ec2.CfnRouteTable(this, 'PrivateRouteTableWithNATGateway', {
+      vpcId: vpc.vpcId,
+    });
+
+    new ec2.CfnRoute(this, 'RouteToNATGateway', {
+      routeTableId: privateRouteTableWithNATGateway.ref,
+      destinationCidrBlock: '0.0.0.0/0',
+      natGatewayId: natGateway.ref,
+    });
+
+    new ec2.CfnSubnetRouteTableAssociation(this, 'PrivateSubnetA1Association', {
+      subnetId: privateSubnetA1.subnetId,
+      routeTableId: privateRouteTableWithNATGateway.ref,
+    });
+
+    new ec2.CfnSubnetRouteTableAssociation(this, 'PrivateSubnetA2Association', {
+      subnetId: privateSubnetA2.subnetId,
+      routeTableId: privateRouteTableWithNATGateway.ref,
+    });
+
+    new cdk.CfnOutput(this, 'VpcId', {
+      value: vpc.vpcId,
+    });
+
+    new cdk.CfnOutput(this, 'InternetGatewayId', {
+      value: internetGateway.ref,
+    });
+
+    new cdk.CfnOutput(this, 'NATGatewayId', {
+      value: natGateway.ref,
     });
   }
 }
